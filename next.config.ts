79678581673dd71e4ded0383ae6fb61a86f3102b
Antonio-Savio <antonio.savio28@gmail.com>
2024-11-22 22:09:41 -0300

    chore: first commit
    - Add initial folder structure
    - Setup dependencies and configs for React, NextJS and Tailwind CSS.
    - Delete default images
    - Add project images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: ''
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: ''
+      },
+      {
+        protocol: 'https',
+        hostname: 'abs.twimg.com',
+        port: ''
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+
+      screens: {
+        'xs': '380px'
+      }
     },
   },
   plugins: [],
