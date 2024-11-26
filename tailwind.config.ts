b530d8e8d4ac926554285ee08f9842a445475a0a
Antonio-Savio <antonio.savio28@gmail.com>
2024-11-26 00:13:01 -0300

    chore: tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,8 +15,21 @@ export default {
 
       screens: {
         'xs': '380px'
+      },
+
+      keyframes: {
+        wave: {
+          "0%, 80%, 100%": { transform: "translateY(0px)" }, //translate-y-0"
+          "40%": { transform: "translateY(0.8rem)" }, //translate-y-0.5
+        },
+      },
+
+      animation: {
+        wave: "wave 0.8s infinite ease",
       }
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
